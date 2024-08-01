ad907aab8e7146b11693d981fe72605bdbe7a5cd
Gita Megantara <110516223+megantaros@users.noreply.github.com>
2024-08-02 02:01:42 +0700

    fix: react player

---
+++ src/components/ui/swiper.tsx
@@ -0,0 +1,91 @@
+'use client'
+
+import React, { FC, memo } from 'react'
+import Slider from 'react-slick';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { ChevronRight } from 'lucide-react';
+
+interface SwiperProps {
+    children?: React.ReactNode
+    infinite?: boolean
+    slidesToShow?: number
+    slidesToScroll?: number
+    dots?: boolean
+    speed?: number
+}
+
+const NextArrow = (props: any) => {
+    const { onClick, currentSlide, slideCount } = props;
+    return (
+        <button
+            className="rounded-full bg-primary p-2 shadow absolute translate-y-1/2 top-20 -right-2 z-50"
+            onClick={onClick}
+            style={{ display: currentSlide === slideCount - 2 || currentSlide === slideCount - 3 ? 'none' : 'block' }}
+        >
+            <ChevronRight size={28} />
+        </button>
+    );
+}
+
+const PrevArrow = (props: any) => {
+    const { onClick, currentSlide } = props;
+    return (
+        <button
+            className="rounded-full bg-primary p-2 shadow absolute translate-y-1/2 top-20 -left-5 z-50"
+            onClick={onClick}
+            style={{ display: currentSlide === 0 ? 'none' : 'block' }}
+        >
+            <ChevronRight size={28} className="rotate-180" />
+        </button>
+    );
+}
+
+const Swiper: FC<SwiperProps> = ({
+    children,
+    infinite = false,
+    slidesToShow = 3,
+    slidesToScroll = 3,
+    dots = true,
+    speed = 500
+}) => {
+    return (
+        <div className="slider-container lg:-mr-3 -mr-[13px]">
+            <Slider
+                initialSlide={0}
+                infinite={infinite}
+                slidesToShow={slidesToShow}
+                slidesToScroll={slidesToScroll}
+                dots={dots}
+                speed={speed}
+                nextArrow={<NextArrow />}
+                prevArrow={<PrevArrow />}
+                responsive={
+                    [
+                        {
+                            breakpoint: 1024,
+                            settings: {
+                                slidesToShow: 3,
+                                slidesToScroll: 2,
+                                dots: true
+                            }
+                        },
+                        {
+                            breakpoint: 480,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 1,
+                                dots: false
+                            }
+                        }
+                    ]
+                }
+            >
+                {children}
+            </Slider>
+        </div>
+    )
+}
+
+export default memo(Swiper)
--- src/features/Tafseer/Tafseer.view.tsx
+++ src/features/Tafseer/Tafseer.view.tsx
@@ -2,10 +2,12 @@ import SearchDrawer from '@/components/search-drawer'
 import { Input } from '@/components/ui/input'
 import Image from 'next/image'
 import Link from 'next/link'
+import YouTube from 'react-youtube'
 
 import { cn } from '@/lib/utils'
 
 import { DM_Serif_Display } from 'next/font/google'
+import Swiper from '@/components/ui/swiper'
 
 const dmSerifDisplay = DM_Serif_Display({
     subsets: ['latin'],
@@ -26,14 +28,27 @@ const TafseerView = () => {
                         className="w-full bg-primary text-background"
                     />
                 </SearchDrawer>
+                <div className="relative z-40 overflow-hidden rounded-[25px] w-full lg:max-w-5xl md:max-w-3xl max-w-sm h-[80vh] flex items-center justify-center mt-16 bg-primary">
+                    <YouTube
+                        videoId="nshdkoeu_fY"
+                        className="absolute inset-0 w-full lg:max-w-5xl md:max-w-3xl max-w-sm h-[80vh]"
+                        opts={{
+                            width: '100%',
+                            height: '100%',
+                            playerVars: {
+                                autoplay: 1
+                            }
+                        }}
+                    />
+                </div>
             </section>
             <section
                 id="latest-tafseer"
-                className="bg-[#191919] py-12 space-y-8 lg:px-8 px-6"
+                className="bg-[#191919] py-12 space-y-8 lg:px-8 px-6 overflow-hidden"
             >
                 <h2 className="font-bold text-2xl">Latest Tafseer</h2>
-                <div className="grid lg:grid-cols-3 grid-cols-1 gap-4">
-                    {[...Array(3)].map((_, i) => (
+                <Swiper>
+                    {[...Array(6)].map((_, i) => (
                         <Link key={i} href="#" id="cardSurah" className="relative overflow-hidden rounded-lg p-6 h-64">
                             <Image
                                 src="/assets/images/tafseer/surah.jpg"
@@ -43,7 +58,7 @@ const TafseerView = () => {
                                 className="w-full h-full absolute inset-0 z-0"
                             />
                             <div className={cn(
-                                "text-center absolute -translate-x-1/2 -translate-y-1/2 top-3/4 left-1/2 z-10",
+                                "text-center absolute -translate-x-1/2 -translate-y-1/2 top-3/4 left-1/2 z-10 w-full max-w-sm",
                                 dmSerifDisplay.className
                             )}
                             >
@@ -52,7 +67,7 @@ const TafseerView = () => {
                             </div>
                         </Link>
                     ))}
-                </div>
+                </Swiper>
             </section>
         </>
     )
