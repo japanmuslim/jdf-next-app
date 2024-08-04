ce8c0acbd9c0a189f7ef79ea496c7564e40592c1
Gita Megantara <110516223+megantaros@users.noreply.github.com>
2024-08-05 03:44:45 +0700

    fix: trigger api

--- src/services/api/https.ts
+++ src/services/api/https.ts
@@ -1,7 +1,7 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 const baseQuery = fetchBaseQuery({ 
-    baseUrl: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8000/api',
+    baseUrl: process.env.NEXT_PUBLIC_API_URL ?? 'https://admin.xn--eckzbvg9a.jp/api/v1/',
 }); 
 
 export default baseQuery;
