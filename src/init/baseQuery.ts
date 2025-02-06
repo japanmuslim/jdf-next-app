9e15f04ad5be2c29091a2a5515e4f377d10226ef
Saifudin Zuhri <22893177+zuhrisaifudin@users.noreply.github.com>
2025-02-06 02:06:03 +0000

    change link api

--- src/init/baseQuery.ts
+++ src/init/baseQuery.ts
@@ -2,7 +2,7 @@ import { fetchBaseQuery } from '@reduxjs/toolkit/query';
 
 const baseQuery = fetchBaseQuery({
   baseUrl:
-    process.env.NEXT_PUBLIC_API_URL ?? 'https://admin.xn--eckzbvg9a.jp/api/v1/',
+    process.env.NEXT_PUBLIC_API_URL ?? 'https://admin.xn--eckzbvg9a9f.jp/api/v1/',
 });
 
 export default baseQuery;
